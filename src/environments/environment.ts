a5f2d5c8aafc27783515e4768ef3173608e455f5
Alan <alanstvn420@gmail.com>
2024-06-08 16:42:14 -0500

    login funcional sin credenciales

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,14 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyBbjBUcNR1GfikXun2dfsVgpPPmJMohBmc",
-    authDomain: "proyecto-43391.firebaseapp.com",
-    databaseURL: "https://proyecto-43391-default-rtdb.firebaseio.com",
-    projectId: "proyecto-43391",
-    storageBucket: "proyecto-43391.appspot.com",
-    messagingSenderId: "46967837061",
-    appId: "1:46967837061:web:e286b6d625af74a3a5b4ec"
+    apiKey: "tus credenciales",
+    authDomain: "tus credenciales",
+    databaseURL: "tus credenciales",
+    projectId: "tus credenciales",
+    storageBucket: "tus credenciales",
+    messagingSenderId: "tus credenciales",
+    appId: "tus credenciales",
+    measurementId: "tus credenciales"
   }
   
 };
